d1b88eb5e2a927684782c5da9d72ae622794f07a
arnavpisces <arnavpisces@gmail.com>
2018-11-22 05:19:33 +0530

    java maven project added

---
+++ OWL/src/owlpackage/First.java
@@ -0,0 +1,23 @@
+package owlpackage;
+import java.io.File;
+import java.util.stream.Stream;
+import org.semanticweb.HermiT.Configuration;
+import org.semanticweb.HermiT.Reasoner;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public OWLOntologyManager manager;
+	}
+
+}
